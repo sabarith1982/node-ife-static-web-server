0c1e7f74191be874ca9df0e49b8c13daadbb658e
Sabarigirivasan Jayaraman <sabarith1982@gmail.com>
2018-07-13 17:19:27 -0700

    Modified popular media

--- server/PopularMedia.js
+++ server/PopularMedia.js
@@ -1,6 +1,170 @@
 const PopularMedia = {
   "code": "200",
   "message": "success",
+  "popularmedia" : [
+					{
+					  "mid": "10010",
+					  "posterImageURL": "images/media/poster/10010.jpg",
+					  "synopsisImageURL": "images/media/synopsis/10010.jpg",
+					  "videoURL": "./videos/sample.mp4",
+					  "title": "Star Wars: Episode VIII: The Last Jedi",
+					  "year": "2017",
+					  "rating": "PG-13",
+					  "description": "As the remnants of the Resistance flee Kylo Ren and the First Order, Rey seeks out Luke Skywalker -- but he wants nothing more to do with the Force.",
+					  "audiotracks": "en, sp",
+					  "closedcaption": "en",
+					  "subtitles": "en, sp",
+					  "trailormid": "100101",
+					  "duration": "0090",
+					  "director": "Rian Johnson",
+					  "cast": "Mark Hamill,Carrie Fisher,Adam Driver",
+					  "userrating": "3.0",
+					  "genres": "Action &amp; Adventure, Children &amp; Family Movies"
+					},
+					{
+					  "mid": "20010",
+					  "posterImageURL": "images/media/poster/20010.jpg",
+					  "synopsisImageURL": "images/media/synopsis/20010.jpg",
+					  "title": "Friends",
+					  "trailorURL": "./videos/trailor.mp4",
+					  "description": "Follows the personal and professional lives of six 20 to 30-something-year-old friends living in Manhattan.",
+					  "creators": "David Crane, Marta Kauffman",
+					  "tvseasons" :
+						  [
+							  {
+								"seasontitle": "Friends - Season 1",
+								"posterImageURL": "/images/posters/friends-season1.jpg",
+								"tvepisodes":[
+									{
+									  "mid": "200101",
+									  "videoURL": "./videos/sample.mp4",
+									  "title": "The One Where Monica Gets a Roommate",
+									  "year": "1994",
+									  "rating": "PG-13",
+									  "description": "Monica and the gang introduce Rachel to the &quot;real world&quot; after she leaves her fiancé at the altar.",
+									  "audiotracks": "en, sp",
+									  "closedcaption": "en",
+									  "subtitles": "en, sp",
+									  "duration": "0023",
+									  "director": "Kevin Bright",
+									  "cast": "Jennifer Aniston, Courteney Cox, Lisa Kudrow...",
+									  "userrating": "4.2",
+									  "genres": "Comedy "
+									},{
+									  "mid": "200102",
+									  "videoURL": "./videos/sample.mp4",
+									  "title": "The One with the Sonogram at the End",
+									  "year": "1994",
+									  "rating": "PG-13",
+									  "description": "Ross finds out his ex-wife is pregnant. Rachel returns her engagement ring to Barry. Monica becomes stressed when her and Ross's parents come to visit.",
+									  "audiotracks": "en, sp",
+									  "closedcaption": "en",
+									  "subtitles": "en, sp",
+									  "duration": "0022",
+									  "director": "Kevin Bright",
+									  "cast": "Jennifer Aniston, Courteney Cox, Lisa Kudrow...",
+									  "userrating": "4.1",
+									  "genres": "Comedy"
+									}
+								]
+							}
+						]
+					},
+					{
+					  "mid": "10011",
+					  "posterImageURL": "images/media/poster/10011.jpg",
+					  "synopsisImageURL": "images/media/synopsis/10011.jpg",
+					  "videoURL": "./videos/sample.mp4",
+					  "title": "Star Wars: Episode VIII: The Last Jedi",
+					  "year": "2017",
+					  "rating": "PG-13",
+					  "description": "As the remnants of the Resistance flee Kylo Ren and the First Order, Rey seeks out Luke Skywalker -- but he wants nothing more to do with the Force.",
+					  "audiotracks": "en, sp",
+					  "closedcaption": "en",
+					  "subtitles": "en, sp",
+					  "trailormid": "100111",
+					  "duration": "0090",
+					  "director": "Rian Johnson",
+					  "cast": "Mark Hamill,Carrie Fisher,Adam Driver",
+					  "userrating": "3.0",
+					  "genres": "Action &amp; Adventure, Children &amp; Family Movies"
+					},
+					{
+					  "mid": "30010",
+					  "posterImageURL": "images/media/poster/30010.jpg",
+					  "synopsisImageURL": "images/media/synopsis/30010.jpg",
+					  "artist": "Michael Jackson",
+					  "album": "Thriller",
+					  "year": "1982",
+					  "rating": "PG-13",
+					  "duration": "0090",
+					  "userrating": "4.9",
+					  "genres": "R&amp;&B &gt; Funk",
+					  "tracks": [
+						{
+						"trackmid": "300101",
+						"tracktitle": "Wanna Be Startin' Somethin'",
+						"duration" : "6:03",
+						"audioURL" : "./audio/300101.mp3"
+						},
+						{
+						"trackmid": "300102",
+						"tracktitle": "Baby Be Mine",
+						"duration" : "4:20",
+						"audioURL" : "./audio/300101.mp3"
+						},
+						{
+						"trackmid": "300103",
+						"tracktitle": "The Girl Is Mine",
+						"duration" : "3:42",
+						"audioURL" : "./audio/300101.mp3"
+						},
+						{
+						"trackmid": "300104",
+						"tracktitle": "Thriller",
+						"duration" : "5:57",
+						"audioURL" : "./audio/300101.mp3"
+						},
+						{
+						"trackmid": "300105",
+						"tracktitle": "Beat It (Single Version)",
+						"duration" : "4:18",
+						"audioURL" : "./audio/300101.mp3"
+						},
+						{
+						"trackmid": "300106",
+						"tracktitle": "Billie Jean",
+						"duration" : "4:54",
+						"audioURL" : "./audio/300101.mp3"
+						},
+						{
+						"trackmid": "300107",
+						"tracktitle": "Human Nature",
+						"duration" : "4:54",
+						"audioURL" : "./audio/300101.mp3"
+						},
+						{
+						"trackmid": "300108",
+						"tracktitle": "P.Y.T. (Pretty Young Thing)",
+						"duration" : "3:59",
+						"audioURL" : "./audio/300101.mp3"
+						}
+					  ]
+					},
+					{
+					  "mid": "40010",
+					  "posterImageURL": "images/media/poster/40010.jpg",
+					  "synopsisImageURL": "images/media/synopsis/40010.jpg",
+					  "title": "Candy Crush Saga",
+					  "gameURL": "/games/candycrush",
+					  "year": "2012",
+					  "rating": "G",
+					  "description": "Candy Crush Saga is a free-to-play match-three puzzle video game released by King on April 12, 2012, for Facebook; other versions for iOS, Android, Windows Phone, and Windows 10 followed. It is a variation of their browser game Candy Crush.",
+					  "creator": "King",
+					  "userrating": "3.0",
+					  "genres": "Puzzle video game"
+					}
+			],
   "data": [
     {
       "mid": "10010",
