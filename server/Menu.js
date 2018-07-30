fd70ba429612da05b2375a159763a4ae7f4ed113
Sabarigirivasan Jayaraman <sabarith1982@gmail.com>
2018-07-30 16:34:55 -0700

    Modified

--- server/Menu.js
+++ server/Menu.js
@@ -1,234 +1,236 @@
 const Menu = {
-  "code": "200",
-  "message": "success",
-  "dropdownMenu": [
-    {
-      "menuID": "DM001",
-      "text": "Home",
-      "iconURL": ""
-    },
-    {
-      "menuID": "DM002",
-      "text": "Connect",
-      "iconURL": ""
-    },
-    {
-      "menuID": "DM003",
-      "text": "Alert",
-      "iconURL": ""
-    },
-    {
-      "menuID": "DM004",
-      "text": "Profile",
-      "iconURL": ""
-    },
-    {
-      "menuID": "DM005",
-      "text": "Feedback",
-      "iconURL": ""
-    },
-    {
-      "menuID": "DM006",
-      "text": "My Media Library",
-      "iconURL": ""
-    },
-    {
-      "menuID": "DM007",
-      "text": "About",
-      "iconURL": ""
-    },
-    {
-      "menuID": "DM008",
-      "text": "Help",
-      "iconURL": ""
-    },
-    {
-      "menuID": "DM009",
-      "text": "Settings",
-      "iconURL": ""
-    }
-  ],
-  "data": [
-    {
-      "menuID": "M001",
-      "text": "Watch",
-      "iconURL": "images/menu/watch.ico",
-      "type": "folder",
-      "menuitems": [
-        {
-          "menuitemID": "M001-1",
-          "text": "Movies",
-          "iconURL": "images/menu/movies.ico",
-          "type": "page"
-        },
-        {
-          "menuitemID": "M001-2",
-          "text": "TV",
-          "iconURL": "images/menu/tv.ico",
-          "type": "page"
-        },
-        {
-          "menuitemID": "M001-3",
-          "text": "Music Videos",
-          "iconURL": "images/menu/musicvideos.ico",
-          "type": "page"
-        },
-        {
-          "menuitemID": "M001-4",
-          "text": "News",
-          "iconURL": "images/menu/news.ico",
-          "type": "page"
-        },
-        {
-          "menuitemID": "M001-5",
-          "text": "Sport",
-          "iconURL": "images/menu/sport.ico",
-          "type": "page"
-        }
-      ]
-    },
-    {
-      "menuID": "M002",
-      "text": "Listen",
-      "iconURL": "images/menu/listen.ico",
-      "type": "folder",
-      "menuitems": [
-        {
-          "menuitemID": "M002-1",
-          "text": "Music",
-          "iconURL": "images/menu/music.ico",
-          "type": "page"
-        },
-        {
-          "menuitemID": "M002-2",
-          "text": "Audio Books",
-          "iconURL": "images/menu/audiobooks.ico",
-          "type": "page"
-        }
-      ]
-    },
-    {
-      "menuID": "M003",
-      "text": "Read",
-      "iconURL": "images/menu/read.ico",
-      "type": "page"
-    },
-    {
-      "menuID": "M006",
-      "text": "Map",
-      "iconURL": "images/menu/map.ico",
-      "type": "page"
-    },
-    {
-      "menuID": "M004",
-      "text": "Surf",
-      "iconURL": "images/menu/surf.ico",
-      "type": "link",
-      "URL": ""
-    },
-    {
-      "menuID": "M005",
-      "text": "Play",
-      "iconURL": "images/menu/play.ico",
-      "type": "page"
-    },
-    {
-      "menuID": "M007",
-      "text": "Shop",
-      "iconURL": "images/menu/shop.ico",
-      "type": "page"
-    },
-    {
-      "menuID": "M008",
-      "text": "Eat",
-      "iconURL": "images/menu/eat.ico",
-      "type": "page"
-    },
-    {
-      "menuID": "M011",
-      "text": "Message",
-      "iconURL": "images/menu/message.ico",
-      "type": "folder",
-      "menuitems": [
-        {
-          "menuitemID": "M011-1",
-          "text": "Chat",
-          "iconURL": "images/menu/chat.ico",
-          "type": "page"
-        },
-        {
-          "menuitemID": "M011-2",
-          "text": "Crew Messages",
-          "iconURL": "images/menu/messages.ico",
-          "type": "page"
-        }
-      ]
-    },
-    {
-      "menuID": "M009",
-      "text": "Kids",
-      "iconURL": "images/menu/kids.ico",
-      "type": "page"
-    },
-    {
-      "menuID": "M010",
-      "text": "Experience",
-      "iconURL": "images/menu/experience.ico",
-      "type": "folder",
-      "menuitems": [
-        {
-          "menuitemID": "M010-1",
-          "text": "About Xiamen Air",
-          "iconURL": "images/menu/aboutxiamen.ico",
-          "type": "page"
-        },
-        {
-          "menuitemID": "M010-2",
-          "text": "Egret Club",
-          "iconURL": "images/menu/egretclub.ico",
-          "type": "page"
-        },
-        {
-          "menuitemID": "M010-3",
-          "text": "Destinations",
-          "iconURL": "images/menu/destinations.ico",
-          "type": "page"
-        },
-        {
-          "menuitemID": "M010-4",
-          "text": "Survey",
-          "iconURL": "images/menu/survey.ico",
-          "type": "page"
-        }
-      ]
-    },
-    {
-      "menuID": "M012",
-      "text": "Settings",
-      "iconURL": "images/menu/settings.ico",
-      "type": "folder",
-      "menuitems": [
-        {
-          "menuitemID": "M012-1",
-          "text": "Call FA",
-          "iconURL": "images/menu/callfa.ico",
-          "type": "page"
-        },
-        {
-          "menuitemID": "M012-2",
-          "text": "Light Control",
-          "iconURL": "images/menu/lightcontrol.ico",
-          "type": "page"
-        },
-        {
-          "menuitemID": "M012-3",
-          "text": "Languages",
-          "iconURL": "images/menu/languages.ico",
-          "type": "page"
-        }
-      ]
-    }
-  ]
+  "lmenu": {
+    "code": "200",
+    "message": "success",
+    "dropdownMenu": [
+      {
+        "menuID": "DM001",
+        "text": "hometxt",
+        "iconURL": ""
+      },
+      {
+        "menuID": "DM002",
+        "text": "connecttxt",
+        "iconURL": ""
+      },
+      {
+        "menuID": "DM003",
+        "text": "alerttxt",
+        "iconURL": ""
+      },
+      {
+        "menuID": "DM004",
+        "text": "profiletxt",
+        "iconURL": ""
+      },
+      {
+        "menuID": "DM005",
+        "text": "feedbacktxt",
+        "iconURL": ""
+      },
+      {
+        "menuID": "DM006",
+        "text": "mymedialibrarytxt",
+        "iconURL": ""
+      },
+      {
+        "menuID": "DM007",
+        "text": "abouttxt",
+        "iconURL": ""
+      },
+      {
+        "menuID": "DM008",
+        "text": "helptxt",
+        "iconURL": ""
+      },
+      {
+        "menuID": "DM009",
+        "text": "settingstxt",
+        "iconURL": ""
+      }
+    ],
+    "data": [
+      {
+        "menuID": "M001",
+        "text": "watchtxt",
+        "iconURL": "images/menu/watch.ico",
+        "type": "folder",
+        "menuitems": [
+          {
+            "menuitemID": "M001-1",
+            "text": "moviestxt",
+            "iconURL": "images/menu/movies.ico",
+            "type": "page"
+          },
+          {
+            "menuitemID": "M001-2",
+            "text": "tvtxt",
+            "iconURL": "images/menu/tv.ico",
+            "type": "page"
+          },
+          {
+            "menuitemID": "M001-3",
+            "text": "mvtxt",
+            "iconURL": "images/menu/musicvideos.ico",
+            "type": "page"
+          },
+          {
+            "menuitemID": "M001-4",
+            "text": "newstxt",
+            "iconURL": "images/menu/news.ico",
+            "type": "page"
+          },
+          {
+            "menuitemID": "M001-5",
+            "text": "sporttxt",
+            "iconURL": "images/menu/sport.ico",
+            "type": "page"
+          }
+        ]
+      },
+      {
+        "menuID": "M002",
+        "text": "listentxt",
+        "iconURL": "images/menu/listen.ico",
+        "type": "folder",
+        "menuitems": [
+          {
+            "menuitemID": "M002-1",
+            "text": "musictxt",
+            "iconURL": "images/menu/music.ico",
+            "type": "page"
+          },
+          {
+            "menuitemID": "M002-2",
+            "text": "abookstxt",
+            "iconURL": "images/menu/audiobooks.ico",
+            "type": "page"
+          }
+        ]
+      },
+      {
+        "menuID": "M003",
+        "text": "readtxt",
+        "iconURL": "images/menu/read.ico",
+        "type": "page"
+      },
+      {
+        "menuID": "M006",
+        "text": "maptxt",
+        "iconURL": "images/menu/map.ico",
+        "type": "page"
+      },
+      {
+        "menuID": "M004",
+        "text": "surftxt",
+        "iconURL": "images/menu/surf.ico",
+        "type": "link",
+        "URL": ""
+      },
+      {
+        "menuID": "M005",
+        "text": "playtxt",
+        "iconURL": "images/menu/play.ico",
+        "type": "page"
+      },
+      {
+        "menuID": "M007",
+        "text": "shoptxt",
+        "iconURL": "images/menu/shop.ico",
+        "type": "page"
+      },
+      {
+        "menuID": "M008",
+        "text": "eattxt",
+        "iconURL": "images/menu/eat.ico",
+        "type": "page"
+      },
+      {
+        "menuID": "M011",
+        "text": "talktxt",
+        "iconURL": "images/menu/talk.ico",
+        "type": "folder",
+        "menuitems": [
+          {
+            "menuitemID": "M011-1",
+            "text": "chattxt",
+            "iconURL": "images/menu/chat.ico",
+            "type": "page"
+          },
+          {
+            "menuitemID": "M011-2",
+            "text": "msgtxt",
+            "iconURL": "images/menu/messages.ico",
+            "type": "page"
+          }
+        ]
+      },
+      {
+        "menuID": "M009",
+        "text": "kidstxt",
+        "iconURL": "images/menu/kids.ico",
+        "type": "page"
+      },
+      {
+        "menuID": "M010",
+        "text": "exptxt",
+        "iconURL": "images/menu/experience.ico",
+        "type": "folder",
+        "menuitems": [
+          {
+            "menuitemID": "M010-1",
+            "text": "abouttxt",
+            "iconURL": "images/menu/aboutxiamen.ico",
+            "type": "page"
+          },
+          {
+            "menuitemID": "M010-2",
+            "text": "egrettxt",
+            "iconURL": "images/menu/egretclub.ico",
+            "type": "page"
+          },
+          {
+            "menuitemID": "M010-3",
+            "text": "desttxt",
+            "iconURL": "images/menu/destinations.ico",
+            "type": "page"
+          },
+          {
+            "menuitemID": "M010-4",
+            "text": "surveytxt",
+            "iconURL": "images/menu/survey.ico",
+            "type": "page"
+          }
+        ]
+      },
+      {
+        "menuID": "M012",
+        "text": "settingstxt",
+        "iconURL": "images/menu/settings.ico",
+        "type": "folder",
+        "menuitems": [
+          {
+            "menuitemID": "M012-1",
+            "text": "callfatxt",
+            "iconURL": "images/menu/callfa.ico",
+            "type": "page"
+          },
+          {
+            "menuitemID": "M012-2",
+            "text": "lighttxt",
+            "iconURL": "images/menu/lightcontrol.ico",
+            "type": "page"
+          },
+          {
+            "menuitemID": "M012-3",
+            "text": "langtxt",
+            "iconURL": "images/menu/languages.ico",
+            "type": "page"
+          }
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = {Menu};
